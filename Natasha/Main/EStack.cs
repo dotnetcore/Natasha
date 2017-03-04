b277fd16a5e0f63e9c6fff2c8ecb414de31495cc
Hu <NMSLanX@users.noreply.github.com>
2017-03-05 03:02:23 +0800

    提供栈的复制以及弹出操作

---
+++ Natasha/Main/EStack.cs
@@ -0,0 +1,17 @@
+﻿using Natasha.Cache;
+using System.Reflection.Emit;
+
+namespace Natasha
+{
+    public class EStack
+    {
+        public static void Push()
+        {
+            ThreadCache.GetIL().Emit(OpCodes.Dup);
+        }
+        public static void Pop()
+        {
+            ThreadCache.GetIL().Emit(OpCodes.Pop);
+        }
+    }
+}
