866a819c3c1605d45519727bb41c14562e0e9ee4
Hu <NMSAzulX@users.noreply.github.com>
2018-05-05 11:50:51 +0800

    增加内部类测试

---
+++ analysis/AnalysisReport/InternalLevel/Internal_InternalEntit.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnalysisReport
+{
+    internal class Internal_InternalEntity
+    {
+        public Internal_InternalEntity()
+        {
+            Field = "1";
+            Property = "1";
+            StaticField = "1";
+            StaticProperty = "1";
+            ReadOnlyField = "1";
+        }
+
+        public string GetFiled()
+        {
+            return Field;
+        }
+        internal string Field;
+        internal string Property { get; set; }
+
+        internal static string StaticField;
+        internal static string StaticProperty { get; set; }
+
+        internal const string ConstField = "1";
+
+        internal readonly string ReadOnlyField;
+    }
+}
