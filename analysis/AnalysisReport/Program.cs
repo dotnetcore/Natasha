a4888831d47b4b644dda61109486a1f43366dd85
Hu <16236903+NMSAzulX@users.noreply.github.com>
2018-05-20 00:28:33 +0800

    调整测试结构

--- analysis/AnalysisReport/Program.cs
+++ analysis/AnalysisReport/Program.cs
@@ -6,27 +6,11 @@ namespace AnalysisReport
     {
         static void Main(string[] args)
         {
-            /*
-             * Show<Public_InternalEntity>();
-             * Show<Public_PrivateEntity>();
-             * Show<Public_ProtectedEntity>();
-             * Show<Public_ProtectedInternalEntity>();
-             * Show<Public_PublicEntity>();
-             * Show<Internal_InternalEntity>();
-             * Show<Internal_PrivateEntity>();
-             * Show<Internal_ProtectedEntity>();
-             * Show<Internal_ProtectedInternalEntity>();
-             * Show<Internal_PublicEntity>();
-             */
-            Show<Internal_PublicEntity>();
+            
+
             Console.ReadKey();
         }
 
-        public static void Show<T>()
-        {
-            ReflectionReportor report = new ReflectionReportor(typeof(T));
-            report.Analysis();
-            report.Show();
-        }
+        
     }
 }
