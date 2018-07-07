3eebfe8f9affaa106f2abfd3d452bc9a3d4e8bb9
Hu <16236903+NMSAzulX@users.noreply.github.com>
2018-07-07 14:32:07 +0800

    添加新的测试工程

---
+++ analysis/AnalysisReport_Core1_1/Program.cs
@@ -0,0 +1,13 @@
+﻿using AnalysisUtils;
+using System;
+
+namespace AnalysisReport_Core1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AnalysisTool.Test();
+        }
+    }
+}
