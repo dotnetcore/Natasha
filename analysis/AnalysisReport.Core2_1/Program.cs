73226b1c51470b6ce560683139ee11fd6f500ca2
Hu <16236903+NMSAzulX@users.noreply.github.com>
2018-11-16 22:21:59 +0800

    Add Core2,1 Tes

---
+++ analysis/AnalysisReport.Core2_1/Program.cs
@@ -0,0 +1,13 @@
+﻿using AnalysisUtils;
+using System;
+
+namespace AnalysisReport.Core2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AnalysisTool.Test();
+        }
+    }
+}
