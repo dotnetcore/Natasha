0d0f48d26314b85f129c93a16d03a440587cded4
Hu <16236903+NMSAzulX@users.noreply.github.com>
2019-05-14 20:31:01 +0800

    上传例子

---
+++ Demo/Program.cs
@@ -0,0 +1,35 @@
+﻿using Natasha;
+using System;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             *   在此之前，你需要右键，选择工程文件，在你的.csproj里面 
+             *   
+             *   写上这样一句浪漫的话： 
+             *   
+             *      <PreserveCompilationContext>true</PreserveCompilationContext>
+             */
+            ScriptBuilder maker = new ScriptBuilder();
+
+            var delegateAction = maker
+                .Namespace(typeof(Console))
+                .Param<string>("str1")
+                .Param<string>("str2")
+                .Body(@"
+                    string result = str1 +"" ""+ str2;
+                    Console.WriteLine(result);
+                                               ")
+                .Return();
+
+            ((Action<string, string>)delegateAction)("Hello", "World!");
+
+
+            Console.ReadKey();
+        }
+    }
+}
