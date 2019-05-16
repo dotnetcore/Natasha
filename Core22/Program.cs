ea28965b6758894c4e069da19207c75d5c153800
NMSAzulX <16236903+NMSAzulX@users.noreply.github.com>
2019-05-16 13:31:03 +0800

    不同版本

---
+++ Core22/Program.cs
@@ -0,0 +1,60 @@
+﻿using Natasha;
+using System;
+
+namespace Core22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ScriptBuilder maker = new ScriptBuilder();
+
+            var delegateAction = maker
+                .Namespace(typeof(Console))
+                .Param<string>("str1")
+                .Param<string>("str2")
+                .Body(@"
+                    string result = str1 +"" ""+ str2;
+                    Console.WriteLine(result);
+                                               ")
+                .Return()
+                .Create();
+
+            ((Action<string, string>)delegateAction)("Hello", "World!");
+
+            ScriptBuilder maker2 = new ScriptBuilder();
+            var delegateAction2 = maker2
+                .Namespace(typeof(Console))
+                .Param<string>("str1")
+                .Param<string>("str2")
+                .Body(@"
+                    string result = str1 +"" ""+ str2;
+                    Console.WriteLine(result);
+                                               ")
+                .Return()
+                .Create<Action<string, string>>();
+
+            delegateAction2("Hello", "World!");
+
+            ClassBuilder classBuilder = new ClassBuilder();
+            string text = @"using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+ 
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(""Hello, World!"");
+        }
+    }
+}";
+            Type type = classBuilder.GetType(text);
+            Console.WriteLine(type.Name);
+            Console.ReadKey();
+        }
+    }
+}
