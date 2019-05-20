3d950a57b6d7d2ba13f73c0958baaefc7bc748f6
NMSAzulX <16236903+NMSAzulX@users.noreply.github.com>
2019-05-20 14:45:19 +0800

    引擎模块反解器
    参数反解以及类型反解，并增加UT测试

---
+++ Natasha/Engine/Builder/Reverser/TypeReverser.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Text;
+
+namespace Natasha.Engine.Builder.Reverser
+{
+
+    public static class TypeReverser
+    {
+        public static ConcurrentDictionary<Type, string> _type_mapping;
+        static TypeReverser()
+        {
+            _type_mapping = new ConcurrentDictionary<Type, string>();
+        }
+
+        public static string Get(Type type)
+        {
+            if (!_type_mapping.ContainsKey(type))
+            {
+                _type_mapping[type] = Reverser(type);
+            }
+            return _type_mapping[type];
+        }
+        public static string Reverser(Type type)
+        {
+            string Suffix = string.Empty;
+            while (type.HasElementType)
+            {
+                if (type.IsArray)
+                {
+                    Suffix = "[]";
+                }
+                type = type.GetElementType();
+            }
+            if (type.IsGenericType)
+            {
+                StringBuilder result = new StringBuilder();
+                result.Append($"{type.Name.Split('`')[0]}<");
+                if (type.GenericTypeArguments.Length > 0)
+                {
+                    result.Append(Reverser(type.GenericTypeArguments[0]));
+                    for (int i = 1; i < type.GenericTypeArguments.Length; i++)
+                    {
+                        result.Append(',');
+                        result.Append(Reverser(type.GenericTypeArguments[i]));
+                    }
+                }
+                result.Append('>');
+                result.Append(Suffix);
+                return result.ToString();
+            }
+            else
+            {
+                if (type==typeof(void))
+                {
+                    return "void";
+                }
+                return type.Name + Suffix;
+            }
+        }
+    }
+}
---
+++ NatashaUT/ReverserTest.cs
@@ -0,0 +1,31 @@
+﻿using Natasha.Engine.Builder.Reverser;
+using NatashaUT.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace NatashaUT
+{
+    public class ReverserTest
+    {
+        [Fact(DisplayName = "参数与类型反解 in")]
+        public void TestIn()
+        {
+            var info = typeof(ReverserTestModel).GetMethod("Test1");
+            Assert.Equal("in Rsm<GRsm>",ParameterInfoReverser.Get(info.GetParameters()[0]));
+        }
+        [Fact(DisplayName = "参数与类型反解 out")]
+        public void TestOut()
+        {
+            var info = typeof(ReverserTestModel).GetMethod("Test2");
+            Assert.Equal("out Rsm<Rsm<GRsm>[]>", ParameterInfoReverser.Get(info.GetParameters()[0]));
+        }
+        [Fact(DisplayName = "参数与类型反解 ref")]
+        public void TestRef()
+        {
+            var info = typeof(ReverserTestModel).GetMethod("Test3");
+            Assert.Equal("ref Rsm<Rsm<GRsm>[]>[]", ParameterInfoReverser.Get(info.GetParameters()[0]));
+        }
+    }
+}
