d8ce89c5ccac693d6f6cc57924fd299367bebc76
NMSAzulX <16236903+NMSAzulX@users.noreply.github.com>
2019-05-21 13:42:25 +0800

    修改完善UT测试

--- NatashaUT/DynamicRemoteTest.cs
+++ NatashaUT/DynamicRemoteTest.cs
@@ -7,21 +7,17 @@ namespace NatashaUT
 {
     public class DynamicRemoteTest
     {
-        [Fact(DisplayName = "远程调用--参数")]
+        [Fact(DisplayName = "远程调用--客户端参数")]
         public void RemoteParametersTest()
         {
-            var parameter =new ParametersMaker<RemoteTestModel>();
-            var temp = parameter["HelloString"].Params("hello ","world!");
-            RemoteWritter.ComplieToRemote<RemoteTestModel>();
-            string result = JsonConvert.DeserializeObject<string>(RemoteReader.Invoke(temp));
-            Assert.Equal("hello world!", result);
-
-            parameter = new ParametersMaker<RemoteTestModel>();
-            temp = parameter["HelloInt"].Params(10, 100);
-            int intResult =JsonConvert.DeserializeObject<int>(RemoteReader.Invoke(temp));
-            Assert.Equal(110, intResult);
+            var parameter = new ParametersMaker<RemoteTestModel>();
+            var temp = parameter["HelloString"].Params("hello ", "world!");
+            Assert.Equal("RemoteTestModel", temp.TypeName);
+            Assert.Equal("HelloString", temp.MethodName);
+            Assert.Equal("\"hello \"", temp["str1"]);
+            Assert.Equal("\"world!\"", temp["str2"]);
         }
-        [Fact(DisplayName = "远程调用--委托")]
+        [Fact(DisplayName = "远程调用--服务端委托")]
         public void RemoteDelegateTest()
         {
             RemoteWritter.ComplieToRemote<RemoteTestModel>();
@@ -31,17 +27,37 @@ namespace NatashaUT
             Assert.NotNull(RemoteReader.GetFunc(parameters));
 
         }
-        [Fact(DisplayName = "远程调用--结果")]
+        [Fact(DisplayName = "远程调用--服务端结果")]
         public void RemoteInvokeTest()
         {
             RemoteWritter.ComplieToRemote<RemoteTestModel>();
+
+
             RemoteParameters parameters = new RemoteParameters();
             parameters.TypeName = "RemoteTestModel";
             parameters.MethodName = "HelloString";
             parameters["str1"] = JsonConvert.SerializeObject("hello ");
             parameters["str2"] = JsonConvert.SerializeObject("world!");
+
+
             string result = RemoteReader.Invoke(parameters);
             Assert.Equal("\"hello world!\"", result);
+
+
+
+
+            var parameter = new ParametersMaker<RemoteTestModel>();
+            var temp = parameter["HelloString"].Params("hello ", "world!");
+
+            result = JsonConvert.DeserializeObject<string>(RemoteReader.Invoke(temp));
+            Assert.Equal("hello world!", result);
+
+
+            parameter = new ParametersMaker<RemoteTestModel>();
+            temp = parameter["HelloInt"].Params(10, 100);
+
+            int intResult = JsonConvert.DeserializeObject<int>(RemoteReader.Invoke(temp));
+            Assert.Equal(110, intResult);
         }
     }
 }
