ff421475da35461991799717d98490f57c53a914
NMSAzulX <16236903+NMSAzulX@users.noreply.github.com>
2019-07-04 14:00:38 +0800

    Create SnapshotExtension.cs

---
+++ Natasha/ExtensionApi/SnapshotExtension.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Natasha.Snapshot
+{
+    public static class SnapshotExtension
+    {
+        public static void MakeSnapshot<T>(this T instance)
+        {
+            SnapshotOperator.MakeSnapshot(instance);
+        }
+        public static Dictionary<string,DiffModel> Compare<T>(this T instance)
+        {
+            return SnapshotOperator.Compare(instance);
+        }
+    }
+}
