cab247560051cac8f135c04ae169c7659d3cdf10
NMSAzulX <16236903+NMSAzulX@users.noreply.github.com>
2019-07-26 09:09:23 +0800

    增加注释

--- src/Natasha/Operator/ClassOperator.cs
+++ src/Natasha/Operator/ClassOperator.cs
@@ -5,6 +5,12 @@ namespace Natasha
 {
     public static class ClassOperator
     {
+
+        /// <summary>
+        /// 根据类名获取类，前提类必须是成功编译过的
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
         public static Type GetType(string name)
         {
            return AssemblyOperator.Loader(ScriptComplieEngine.ClassMapping[name])[name];
