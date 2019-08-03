1820f39cd5bde971b3ef170fe3b1bc2a110a9361
Hu <16236903+NMSAzulX@users.noreply.github.com>
2019-08-03 19:15:39 +0800

    添加注释

--- src/Natasha/Engine/ExceptionModule/CompilationException.cs
+++ src/Natasha/Engine/ExceptionModule/CompilationException.cs
@@ -15,18 +15,27 @@ namespace Natasha
         }
 
 
+        //脚本字符串
         public string Source;
+
+        //错误信息
         public string Message;
+
+        //格式化后的脚本字符串
         public string Formatter;
+
+        //错误类型
         public ComplieError ErrorFlag;
+
+        //roslyn诊断集合
         public List<Diagnostic> Diagnostics;
        
-
     }
 
 
 
-    public enum ComplieError {
+    public enum ComplieError
+    {
         None,
         Assembly,
         Type,
