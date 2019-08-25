d563b545efa3056bb4ea0e20e8d24cb5e3a6aaa3
Hu <16236903+NMSAzulX@users.noreply.github.com>
2019-08-25 10:29:31 +0800

    增加解构封装
    调整目录结构

---
+++ src/Natasha/DWrapper/DString.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Loader;
+
+namespace Natasha
+{
+    public static class DString
+    {
+
+
+#if NETCOREAPP3_0
+        public static void Deconstruct(
+            this string path,
+            out Assembly Assembly,
+            out ConcurrentDictionary<string,Type> TypeCache)
+        {
+
+            if (File.Exists(path))
+            {
+                var domain = (AssemblyDomain)(AssemblyLoadContext.CurrentContextualReflectionContext);
+                Assembly = domain.LoadFile(path);
+                TypeCache = new ConcurrentDictionary<string, Type>();
+                foreach (var item in Assembly.GetTypes())
+                {
+                    TypeCache[item.GetDevelopName()] = item;
+                }
+            }
+            else
+            {
+                throw new Exception("Can't find file.");
+            }
+            
+        }
+
+
+
+
+        public static void Deconstruct(
+           this string script,
+           out Type[] Type,
+           out CompilationException Error)
+        {
+
+            AssemblyComplier assembly = new AssemblyComplier();
+            assembly.Add(script);
+            Type = assembly.GetAssembly().GetTypes();
+            Error = assembly.Exception;
+
+        }
+#endif
+
+    }
+
+}
