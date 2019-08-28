d8bb70a096406d3c14006c7766e84f8d8f9b192b
Hu <16236903+NMSAzulX@users.noreply.github.com>
2019-08-28 22:21:54 +0800

    添加插件回收测试

--- test/NatashaUT/AssemblyPluginTest.cs
+++ test/NatashaUT/AssemblyPluginTest.cs
@@ -16,26 +16,25 @@ namespace NatashaUT
 #if !NETCOREAPP2_2
 
 
-        [Fact(DisplayName = "插件编译协作")]
+        [Fact(DisplayName = "不可回收：MySql插件")]
         public void Test1()
         {
 
-            Assert.False(GetResult());
+            Assert.False(GetResult1());
             for (int i = 0; i < 6; i++)
             {
                 GC.Collect();
                 GC.WaitForPendingFinalizers();
             }
-            Assert.True(DomainManagment.IsDeleted("TempDomain11"));
+            Assert.False(DomainManagment.IsDeleted("TempDomain11"));
 
         }
 
         [MethodImpl(MethodImplOptions.NoInlining)]
-        public bool GetResult()
+        public bool GetResult1()
         {
             bool result=false;
-            //string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Lib", "ClassLibrary1.dll");
-            string path = @"I:\Vs2017Test\SelfTestNatasha\ClassLibrary1\bin\Debug\netstandard2.0\ClassLibrary1.dll";
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Lib","Sql", "ClassLibrary1.dll");
             using (DomainManagment.CreateAndLock("TempDomain11"))
             {
 
@@ -45,6 +44,79 @@ namespace NatashaUT
                    .Using(assemebly)
                    .MethodBody(@"Class1 a = new Class1();return  a.Show();")
                    .Complie<Func<bool>>();
+                result = action();
+                domain.Dispose();
+                domain.Unload();
+            }
+            return result;
+        }
+
+
+        [Fact(DisplayName = "可回收：静态引用")]
+        public void Test2()
+        {
+
+            Assert.Equal("11",GetResult2());
+            for (int i = 0; i < 6; i++)
+            {
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+            }
+            Assert.True(DomainManagment.IsDeleted("TempDomain12"));
+
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public string GetResult2()
+        {
+            string result;
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Lib", "Static", "ClassLibrary5.dll");
+            using (DomainManagment.CreateAndLock("TempDomain12"))
+            {
+
+                var domain = DomainManagment.CurrentDomain;
+                var assemebly = domain.LoadFile(path);
+                var action = FastMethodOperator.New
+                   .Using(assemebly)
+                   .MethodBody(@"Test.Instance.Name=""11""; return Test.Instance.Name;")
+                   .Complie<Func<string>>();
+                result = action();
+                domain.Dispose();
+                domain.Unload();
+            }
+            return result;
+        }
+
+
+
+        [Fact(DisplayName = "不可回收：Json.net")]
+        public void Test3()
+        {
+
+            Assert.Equal("{\"Name\":\"11\"}", GetResult3());
+            for (int i = 0; i < 6; i++)
+            {
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+            }
+            Assert.False(DomainManagment.IsDeleted("TempDomain13"));
+
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public string GetResult3()
+        {
+            string result;
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Lib", "Json", "ClassLibrary6.dll");
+            using (DomainManagment.CreateAndLock("TempDomain13"))
+            {
+
+                var domain = DomainManagment.CurrentDomain;
+                var assemebly = domain.LoadFile(path);
+                var action = FastMethodOperator.New
+                   .Using(assemebly)
+                   .MethodBody(@"Class1 obj = new Class1(); return obj.Get();")
+                   .Complie<Func<string>>();
                 result = action();
                 domain.Dispose();
                 domain.Unload();
