7288d6e35f654f33446ab0d35eee357ce32df4af
Hu <16236903+NMSAzulX@users.noreply.github.com>
2019-08-31 20:12:15 +0800

    修改部分API

--- src/Natasha/ExtensionApi/DelegateExtension.cs
+++ src/Natasha/ExtensionApi/DelegateExtension.cs
@@ -5,12 +5,12 @@ namespace Natasha.DelegateExtension
 {
     public static class DelegateExtension
     {
-        public static T Create<T>(this T instance,string content,params Type[] usings) where T: Delegate
+        public static T Create<T>(this T instance,string content,params string[] usings) where T: Delegate
         {
             return instance = DelegateOperator<T>.Delegate(content, usings);
         }
 
-        public static Delegate Create(this Type instance, string content, params Type[] usings)
+        public static Delegate Create(this Type instance, string content, params string[] usings)
         {
             var method = instance.GetMethod("Invoke");
             return FakeMethodOperator
