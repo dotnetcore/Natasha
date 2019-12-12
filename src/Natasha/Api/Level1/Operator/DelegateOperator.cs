92571541df84bba64335708b8b7a83f890a63d81
Hu <16236903+NMSAzulX@users.noreply.github.com>
2019-12-12 12:01:12 +0800

    Update DelegateOperator.cs

--- src/Natasha/Api/Level1/Operator/DelegateOperator.cs
+++ src/Natasha/Api/Level1/Operator/DelegateOperator.cs
@@ -5,6 +5,70 @@ namespace Natasha.Operator
     public static class DelegateOperator<T> where T : Delegate
     {
 
+        public static T Delegate(string content, params NamespaceConverter[] usings)
+        {
+
+            var method = typeof(T).GetMethod("Invoke");
+            return FakeMethodOperator.Default
+                .UseMethod(method)
+                .Using(usings)
+                .StaticMethodContent(content)
+                .Complie<T>();
+
+        }
+
+
+
+
+        public static T AsyncDelegate(string content, params NamespaceConverter[] usings)
+        {
+
+            var method = typeof(T).GetMethod("Invoke");
+            return FakeMethodOperator.Default
+                .UseMethod(method)
+                .UseAsync()
+                .Using(usings)
+                .StaticMethodContent(content)
+                .Complie<T>();
+
+        }
+
+
+
+
+        public static T UnsafeDelegate(string content, params NamespaceConverter[] usings)
+        {
+
+            var method = typeof(T).GetMethod("Invoke");
+            return FakeMethodOperator.Default
+                .UseMethod(method)
+                .UseUnsafe()
+                .Using(usings)
+                .StaticMethodContent(content)
+                .Complie<T>();
+
+        }
+
+
+
+
+        public static T UnsafeAsyncDelegate(string content, params NamespaceConverter[] usings)
+        {
+
+            var method = typeof(T).GetMethod("Invoke");
+            return FakeMethodOperator.Default
+                .UseMethod(method)
+                .UseUnsafe()
+                .UseAsync()
+                .Using(usings)
+                .StaticMethodContent(content)
+                .Complie<T>();
+
+        }
+
+
+
+
         public static T Delegate(string content, AssemblyDomain domain = default, bool inCache = false,bool complieInFile = false, params NamespaceConverter[] usings)
         {
 
