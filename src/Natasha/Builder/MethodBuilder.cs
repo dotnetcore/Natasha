880c7c94dd92bb4d618506db323d6d2cae788c7c
weihanli <weihanli@outlook.com>
2020-04-22 11:43:00 +0800

    fix DynamicMethodTest remove GetHashCode test code in MethodBuilder

--- src/Natasha/Builder/MethodBuilder.cs
+++ src/Natasha/Builder/MethodBuilder.cs
@@ -1,97 +1,68 @@
-﻿using Natasha.Error;
-using Natasha.CSharp.Template;
+﻿using Natasha.CSharp.Template;
+using Natasha.Error;
 using System;
 
 namespace Natasha.CSharp.Builder
 {
-
     public class MethodBuilder : MethodBuilder<MethodBuilder>
     {
-
         public MethodBuilder()
         {
             Link = this;
         }
-
     }
-
 
     public class MethodBuilder<T> : DelegateTemplate<T> where T : MethodBuilder<T>, new()
     {
-
-
         private readonly OopBuilder _oopHandler;
         public CompilationException Exception;
 
         public MethodBuilder()
         {
-
             _oopHandler = new OopBuilder();
             Body(default);
             Init();
-
         }
-
-
-
 
         public T Using<S>()
         {
-
             _oopHandler.Using<S>();
             return Link;
-
         }
+
         public T Using(Type @using)
         {
-
             _oopHandler.Using(@using);
             return Link;
-
         }
+
         public T Using(string @using)
         {
-
             _oopHandler.Using(@using);
             return Link;
-
         }
+
         public T Using(NamespaceConverter @using)
         {
-
             _oopHandler.Using(@using);
             return Link;
-
         }
+
         public T Using(NamespaceConverter[] @using)
         {
-
             _oopHandler.Using(@using);
             return Link;
-
         }
-
-
-
 
         public virtual void Init()
         {
-
         }
-
-
-
 
         public T ClassOptions(Action<OopBuilder> acion)
         {
-
             acion?.Invoke(_oopHandler);
             return Link;
-
         }
-
-
-
 
         /// <summary>
         /// 设置类名
@@ -100,14 +71,9 @@ namespace Natasha.CSharp.Builder
         /// <returns></returns>
         public T ClassName(string className)
         {
-
             _oopHandler.DefinedName(className);
             return Link;
-
         }
-
-
-
 
         /// <summary>
         /// 设置命名空间
@@ -116,23 +82,16 @@ namespace Natasha.CSharp.Builder
         /// <returns></returns>
         public T Namesapce(string @namespace)
         {
-
             _oopHandler.Namespace(@namespace);
             return Link;
-
         }
-
-
-
 
         public Delegate Compile(object binder = null)
         {
-
             if (_oopHandler.NamespaceScript == default)
             {
                 _oopHandler.HiddenNamespace();
             }
-
 
             _oopHandler.Body(Script);
             Exception = AssemblyBuilder.Syntax.Add(_oopHandler);
@@ -146,50 +105,34 @@ namespace Natasha.CSharp.Builder
                 return @delegate;
             }
             return null;
-
         }
-
-
-
-
 
         public S Compile<S>(object binder = null) where S : Delegate
         {
-
-
             if (_oopHandler.NamespaceScript == default)
             {
-
                 _oopHandler.HiddenNamespace();
-
             }
-
 
             //自动判别是否有手动指定方法参数，若没有则使用方法的参数
             var method = typeof(S).GetMethod("Invoke");
             if (ParametersScript.Length == 1)
             {
-
                 Param(method);
-
             }
-
 
             //自动判别返回值类型，若没有则使用委托的返回类型
             if (_type != method.ReturnType)
             {
-
                 this.Return(method.ReturnType);
-
             }
-
 
             _oopHandler.Body(Script);
             Exception = AssemblyBuilder.Syntax.Add(_oopHandler);
             if (!Exception.HasError)
             {
                 S @delegate = AssemblyBuilder.GetDelegateFromShortName<S>(_oopHandler.NameScript, NameScript, binder);
-                var a = @delegate.GetHashCode();
+
                 if (@delegate == null)
                 {
                     Exception = AssemblyBuilder.Exceptions[0];
@@ -197,9 +140,6 @@ namespace Natasha.CSharp.Builder
                 return @delegate;
             }
             return null;
-
         }
-
     }
-
 }
--- test/NatashaUT/DynamicMethodTest.cs
+++ test/NatashaUT/DynamicMethodTest.cs
@@ -6,10 +6,9 @@ using Xunit;
 
 namespace NatashaUT
 {
-    [Trait("快速构建","函数")]
+    [Trait("快速构建", "函数")]
     public class DynamicMethodTest
-    { 
-
+    {
         [Fact(DisplayName = "手动强转委托")]
         public static void RunDelegate1()
         {
@@ -24,11 +23,9 @@ namespace NatashaUT
                         .Return<string>()
                 .Compile();
 
-           string result = ((Func<string, string, string>)delegateAction)?.Invoke("Hello", "World1!");
-           Assert.Equal("Hello World1!", result);
+            string result = ((Func<string, string, string>)delegateAction)?.Invoke("Hello", "World1!");
+            Assert.Equal("Hello World1!", result);
         }
-
-
 
         [Fact(DisplayName = "内部类委托")]
         public static void RunInnerDelegate()
@@ -46,38 +43,33 @@ namespace NatashaUT
             Assert.Equal("abc", result.Name);
         }
 
-
-
         [Fact(DisplayName = "NFunc委托")]
         public static void RunDelegate5()
         {
-
             NSucceedLog.Enabled = true;
-            Func<string, string, string> action = NDelegate.Random(builder=>builder.UseFileCompile()).UnsafeFunc<string, string, string>(@"
+            Func<string, string, string> action = NDelegate.Random(builder => builder.UseFileCompile())
+                .UnsafeFunc<string, string, string>(@"
                             string result = arg1 +"" ""+ arg2;
                             Console.WriteLine(result);
                             return result;");
 
-            Func<string, string, string> action2 = NDelegate.Random(builder => builder.UseFileCompile()).UnsafeFunc<string, string, string>(@"
+            Func<string, string, string> action2 = NDelegate.Random(builder => builder.UseFileCompile())
+                .UnsafeFunc<string, string, string>(@"
                             string result = arg1 + "" "" + arg2 + ""1"";
                             Console.WriteLine(result);
                             return result;");
 
-            Func<string, string, string> action3 = (s1, s2) => s1 + s2;
+            Assert.NotEqual(action.Method.GetHashCode(), action2.Method.GetHashCode());
+            // Assert.NotEqual(action.GetHashCode(), action2.GetHashCode());
 
+            Func<string, string, string> action3 = (s1, s2) => s1 + s2;
 
             string result = action("Hello", "World1!");
             Assert.Equal("Hello World1!", result);
 
-
-            Assert.NotEqual(action3.GetHashCode(), action2.GetHashCode());
-            Assert.NotEqual(action.Method.GetHashCode(), action2.Method.GetHashCode());
-            Assert.NotEqual(action.GetHashCode(), action2.GetHashCode());
-
+            Assert.NotEqual(action3.Method.GetHashCode(), action2.Method.GetHashCode());
+            //Assert.NotEqual(action3.GetHashCode(), action2.GetHashCode());
         }
-
-
-
 
         [Fact(DisplayName = "自动泛型委托")]
         public static void RunDelegate2()
@@ -94,9 +86,7 @@ namespace NatashaUT
                 .Compile<Func<string, string, string>>();
 
             string result = delegateAction?.Invoke("Hello", "World2!");
-           Assert.Equal("Hello World2!",result);
+            Assert.Equal("Hello World2!", result);
         }
-
     }
-
 }
