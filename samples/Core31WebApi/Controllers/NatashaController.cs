410575556560a92a0270672f5105824dacfad749
NMSAzulX <16236903+NMSAzulX@users.noreply.github.com>
2020-05-14 17:28:46 +0800

    测试动态 API

---
+++ samples/Core31WebApi/Controllers/NatashaController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Natasha;
+using Natasha.CSharp;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Core31WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class NatashaController : Controller
+    {
+
+        // GET api/<controller>/5
+        [HttpGet("{code}")]
+        public string Get(string code)
+        {
+
+            AssemblyCSharpBuilder builder = new AssemblyCSharpBuilder();
+            builder.Compiler.Domain = DomainManagement.Default;
+            builder.Syntax.Add(code);
+            builder.GetAssembly();
+            return "succeed!";
+
+        }
+
+       
+    }
+}
--- samples/Core31WebApi/Startup.cs
+++ samples/Core31WebApi/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Natasha;
 
 namespace Core31WebApi
 {
@@ -26,18 +28,33 @@ namespace Core31WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            DomainManagement.RegisterDefault<AssemblyDomain>();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseSwagger();
 
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+
+            });
             app.UseHttpsRedirection();
-
+            
             app.UseRouting();
 
             app.UseAuthorization();
