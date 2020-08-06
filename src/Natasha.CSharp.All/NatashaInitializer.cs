737960bb1c76289160641e94896c92360ef335c0
huxin <16236903+NMSAzulX@users.noreply.github.com>
2020-08-06 10:54:20 +0800

    增加新项目

---
+++ src/Natasha.CSharp.All/NatashaInitializer.cs
@@ -0,0 +1,30 @@
+﻿using Natasha.CSharp;
+using System;
+using System.Threading.Tasks;
+
+public class NatashaInitializer
+{
+
+    /// <summary>
+    /// 初始化 Natasha 组件
+    /// </summary>
+    public static void Initialize()
+    {
+        NatashaComponentRegister.RegistDomain<NatashaAssemblyDomain>();
+        NatashaComponentRegister.RegistCompiler<NatashaCSharpCompiler>();
+        NatashaComponentRegister.RegistSyntax<NatashaCSharpSyntax>();
+    }
+
+    /// <summary>
+    /// 初始化 Natasha 组件并预热
+    /// </summary>
+    /// <returns></returns>
+    public static async Task InitializeAndPreheating()
+    {
+        Initialize();
+        var action = NDelegate.RandomDomain().Action("");
+        action();
+        action.DisposeDomain();
+    }
+}
+
