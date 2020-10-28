1d3a53492842b3dc17302ffbb6e1fb61b3dd552c
AzulX <16236903+NMSAzulX@users.noreply.github.com>
2020-10-28 16:41:59 +0800

    支持 .netcore3.1 runtime

--- samples/Core31/Program.cs
+++ samples/Core31/Program.cs
@@ -20,27 +20,33 @@ namespace Core31
             //    .Compile<Func<string, string, string>>();
             //Console.WriteLine(hwFunc("Hello", " World!"));
 
+            
+            string temp = NDelegate.RandomDomain().Func<string>("return (new A()).Name;")();
+            Console.WriteLine(temp);
 
-            var a123 = NClass.UseDomain(typeof(Program).GetDomain());
-            var domain = DomainManagement.Random;
-            var type = NDelegate.UseDomain(domain,item=>item.AssemblyName="a").GetType($"[assembly: AssemblyKeyFileAttribute(\"c:\\\\vs2019\\\\natasha.snk\")]" +"[assembly: AssemblyVersion(\"1.3.3.3\")]public class A{ public A(){Name=\"1\"; }public string Name;}");
-            var func = NDelegate.UseDomain(domain).Func<string>("return (new A()).Name;");
-            Console.WriteLine(type.FullName);
-            Console.WriteLine(func());
+            //var a123 = NClass.UseDomain(typeof(Program).GetDomain());
+            //var domain = DomainManagement.Random;
+            //var type = NDelegate.UseDomain(domain,item=>item.AssemblyName="a").GetType($"[assembly: AssemblyKeyFileAttribute(\"c:\\\\vs2019\\\\natasha.snk\")]" +"[assembly: AssemblyVersion(\"1.3.3.3\")]public class A{ public A(){Name=\"1\"; }public string Name;}");
+            //var func = NDelegate.UseDomain(domain).Func<string>("return (new A()).Name;");
+            //Console.WriteLine(type.FullName);
+            //Console.WriteLine(func());
 
-            //type.RemoveReferences();
-            type = NDelegate.UseDomain(domain,item=>item.AssemblyName="a").GetType($"[assembly: AssemblyKeyFileAttribute(\"c:\\\\vs2019\\\\natasha.snk\")]" + "[assembly: AssemblyVersion(\"2.3.3.4\")]public class A{ public A(){Name=\"2\"; }public string Name;}");
-            func = NDelegate.UseDomain(domain).Func<string>("return (new A()).Name;");
-            Console.WriteLine(type.FullName);
-            Console.WriteLine(func());
+            ////type.RemoveReferences();
+            //type = NDelegate.UseDomain(domain,item=>item.AssemblyName="a").GetType($"[assembly: AssemblyKeyFileAttribute(\"c:\\\\vs2019\\\\natasha.snk\")]" + "[assembly: AssemblyVersion(\"2.3.3.4\")]public class A{ public A(){Name=\"2\"; }public string Name;}");
+            //func = NDelegate.UseDomain(domain).Func<string>("return (new A()).Name;");
+            //Console.WriteLine(type.FullName);
+            //Console.WriteLine(func());
 
-            domain = DomainManagement.Create("a");
-            using (DomainManagement.Lock("a"))
-            {
-                Console.WriteLine(domain == (NatashaAssemblyDomain)AssemblyLoadContext.CurrentContextualReflectionContext);
-            }
+            //domain = DomainManagement.Create("a");
+            //using (DomainManagement.Lock("a"))
+            //{
+            //    Console.WriteLine(domain == (NatashaAssemblyDomain)AssemblyLoadContext.CurrentContextualReflectionContext);
+            //}
 
             Console.ReadKey();
         }
+
+
     }
+
 }
--- src/Natasha.CSharp/Natasha.CSharp.All/NatashaInitializer.cs
+++ src/Natasha.CSharp/Natasha.CSharp.All/NatashaInitializer.cs
@@ -5,19 +5,22 @@ using System.Threading.Tasks;
 public static class NatashaInitializer
 {
     private static bool _hasInitialize;
+    private static readonly object _lock;
+
+    static NatashaInitializer()
+    {
+        NatashaComponentRegister.RegistDomain<NatashaAssemblyDomain>();
+        NatashaComponentRegister.RegistCompiler<NatashaCSharpCompiler>();
+        NatashaComponentRegister.RegistSyntax<NatashaCSharpSyntax>();
+        _lock = new object();
+    }
+
     /// <summary>
     /// 初始化 Natasha 组件
     /// </summary>
     public static async Task Initialize()
     {
 
-        if (!_hasInitialize)
-        {
-            NatashaComponentRegister.RegistDomain<NatashaAssemblyDomain>();
-            NatashaComponentRegister.RegistCompiler<NatashaCSharpCompiler>();
-            NatashaComponentRegister.RegistSyntax<NatashaCSharpSyntax>();
-            _hasInitialize = true;
-        }
 
     }
 
@@ -30,12 +33,18 @@ public static class NatashaInitializer
 
         if (!_hasInitialize)
         {
-            Initialize();
-            var action = NDelegate.RandomDomain().Action("");
-            action();
-            action.DisposeDomain();
+            lock (_lock)
+            {
+                if (!_hasInitialize)
+                {
+                    _hasInitialize = true;
+                    var action = NDelegate.RandomDomain().Action("");
+                    action();
+                    action.DisposeDomain();
+                }
+            }
         }
-
+            
     }
 }
 
