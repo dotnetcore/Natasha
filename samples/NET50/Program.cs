1295bde08326e08aaefae81eb2c7d3f9440d4825
AzulX <16236903+NMSAzulX@users.noreply.github.com>
2020-11-11 12:08:45 +0800

    增加 .NET5 测试程序

---
+++ samples/NET50/Program.cs
@@ -0,0 +1,15 @@
+﻿using Natasha.CSharp;
+using System;
+
+namespace NET50
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NatashaInitializer.InitializeAndPreheating();
+            NDelegate.RandomDomain().Action("Console.WriteLine(\"Hello World!\");")();
+            Console.ReadKey();
+        }
+    }
+}
