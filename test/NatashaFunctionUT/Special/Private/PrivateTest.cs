061e38d4270dd0f85434ff92cbc8cb14e518bacf
AzulX <16236903+NMSAzulX@users.noreply.github.com>
2022-01-12 17:40:57 +0800

    re

--- test/NatashaFunctionUT/Special/Private/PrivateTest.cs
+++ test/NatashaFunctionUT/Special/Private/PrivateTest.cs
@@ -11,13 +11,12 @@ namespace NatashaFunctionUT.Special
         {
             PrivateMemberClassModel test = new PrivateMemberClassModel();
             var action = NDelegate
-                .RandomDomain()
-                .ConfigClass(item=>item.AllowPrivate<PrivateMemberClassModel>())
+                .RandomDomain(builder => builder.ConfigCompilerOption(opt => opt.SetCompilerFlag(Natasha.CSharp.Compiler.CompilerBinderFlags.IgnoreAccessibility | Natasha.CSharp.Compiler.CompilerBinderFlags.IgnoreCorLibraryDuplicatedTypes)))
+                .ConfigClass(item => item.AllowPrivate<PrivateMemberClassModel>())
                 .Action<PrivateMemberClassModel>("obj.publicA = 2;obj.internalA = 3; obj.privateA=1;");
             action(test);
-#if DEBUG
-        Assert.Equal(4, test.GetD);
-#endif
+            Assert.Equal(4, test.GetD);
+
 
         }
 
@@ -28,13 +27,12 @@ namespace NatashaFunctionUT.Special
 
             PrivateMemberClassModel test = new PrivateMemberClassModel();
             var action = NDelegate
-                .RandomDomain()
-                .ConfigClass(item => item.AllowPrivate<PrivateMemberClassModel>().SkipInit())
+                .RandomDomain(builder => builder.ConfigCompilerOption(opt => opt.SetCompilerFlag(Natasha.CSharp.Compiler.CompilerBinderFlags.IgnoreAccessibility | Natasha.CSharp.Compiler.CompilerBinderFlags.IgnoreCorLibraryDuplicatedTypes)))
+                .ConfigClass(item => item.AllowPrivate<PrivateMemberClassModel>())
                 .Action<PrivateMemberClassModel>("obj.publicA = 2;obj.internalA = 3; obj.privateA=1;");
             action(test);
-#if DEBUG
             Assert.Equal(4, test.GetD);
-#endif
+
         }
 
 
