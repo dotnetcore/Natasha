63c4163b5235d5fd4a1c8b5969839e0c1c3e1d75
AzulX <16236903+NMSAzulX@users.noreply.github.com>
2022-01-12 16:38:00 +0800

    移除无用项目

--- test/NormalBenchmark/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using BenchmarkDotNet.Running;
-using NatashaBenchmark;
-
-BenchmarkRunner.Run<UnsafeDelegate>();
-Console.ReadKey();
