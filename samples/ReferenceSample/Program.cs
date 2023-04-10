db1d4aaf106b764addbb1ba96401b9faa8aa284c
AzulX <16236903+NMSAzulX@users.noreply.github.com>
2023-04-10 14:07:39 +0800

    create new sample

---
+++ samples/ReferenceSample/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ReferenceSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NatashaManagement.Preheating();
+            Console.ReadKey();
+        }
+    }
+}
