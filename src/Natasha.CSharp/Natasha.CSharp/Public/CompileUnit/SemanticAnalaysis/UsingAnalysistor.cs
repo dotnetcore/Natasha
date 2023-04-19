178d01680778f2e3bcd4f728d299d05c9df289ed
AzulX <16236903+NMSAzulX@users.noreply.github.com>
2023-04-19 18:28:22 +0800

    格式修正

--- src/Natasha.CSharp/Natasha.CSharp/Public/CompileUnit/SemanticAnalaysis/UsingAnalysistor.cs
+++ src/Natasha.CSharp/Natasha.CSharp/Public/CompileUnit/SemanticAnalaysis/UsingAnalysistor.cs
@@ -17,76 +17,76 @@ namespace Natasha.CSharp.Compiler.SemanticAnalaysis
             _usingSemanticDelegate = (builder, compilation, ignoreAccessibility) =>
             {
 
-               var trees = compilation.SyntaxTrees;
-               foreach (var tree in trees)
-               {
+                var trees = compilation.SyntaxTrees;
+                foreach (var tree in trees)
+                {
 #if DEBUG
-                   Stopwatch stopwatch = new();
-                   stopwatch.Start();
+                    Stopwatch stopwatch = new();
+                    stopwatch.Start();
 #endif
-                   CompilationUnitSyntax root = tree.GetCompilationUnitRoot();
-                   
-                   SemanticModel semantiModel = compilation.GetSemanticModel(tree, ignoreAccessibility);
+                    CompilationUnitSyntax root = tree.GetCompilationUnitRoot();
+
+                    SemanticModel semantiModel = compilation.GetSemanticModel(tree, ignoreAccessibility);
 #if DEBUG
                     stopwatch.RestartAndShowCategoreInfo("[Semantic]", "语义节点获取", 3);
 #endif
-                 
-                   var errors = semantiModel.GetDiagnostics();
+
+                    var errors = semantiModel.GetDiagnostics();
 #if DEBUG
-                   stopwatch.StopAndShowCategoreInfo("[Semantic]", "语义诊断获取", 3);
-                   stopwatch.Restart();
+                    stopwatch.StopAndShowCategoreInfo("[Semantic]", "语义诊断获取", 3);
+                    stopwatch.Restart();
 #endif
 
-                   if (errors.Length > 0)
-                   {
-                       var errorNodes = new HashSet<SyntaxNode>();
-                       for (int i = 0; i < errors.Length; i++)
-                       {
-                           var error = errors[i];
-                           if (error.Id == "CS0434")
-                           {
-                               error.RemoveDefaultUsingAndUsingNode(root, errorNodes);
-                           }
-                           else if (error.Id == "CS8019")
-                           {
+                    if (errors.Length > 0)
+                    {
+                        var errorNodes = new HashSet<SyntaxNode>();
+                        for (int i = 0; i < errors.Length; i++)
+                        {
+                            var error = errors[i];
+                            if (error.Id == "CS0434")
+                            {
+                                error.RemoveDefaultUsingAndUsingNode(root, errorNodes);
+                            }
+                            else if (error.Id == "CS8019")
+                            {
                                 var node = error.GetTypeSyntaxNode<UsingDirectiveSyntax>(root);
                                 if (node != null)
                                 {
                                     errorNodes.Add(node);
                                 }
-                              
-                           }
-                           else if (error.Id == "CS0246")
-                           {
-                               var node = error.GetTypeSyntaxNode<UsingDirectiveSyntax>(root);
-                               if (node != null)
-                               {
-                                   NatashaDiagnosticsExtension.RemoveUsingAndNode(node, errorNodes);
-                               }
-                           }
-                           else if (error.Id == "CS0234")
-                           {
-                               error.RemoveUsingAndNodesFromStartName(root, errorNodes);
-                           }
-                       }
+
+                            }
+                            else if (error.Id == "CS0246")
+                            {
+                                var node = error.GetTypeSyntaxNode<UsingDirectiveSyntax>(root);
+                                if (node != null)
+                                {
+                                    NatashaDiagnosticsExtension.RemoveUsingAndNode(node, errorNodes);
+                                }
+                            }
+                            else if (error.Id == "CS0234")
+                            {
+                                error.RemoveUsingAndNodesFromStartName(root, errorNodes);
+                            }
+                        }
 
 #if DEBUG
-                       stopwatch.RestartAndShowCategoreInfo("[Semantic]", "语义节点筛查", 3);
+                        stopwatch.RestartAndShowCategoreInfo("[Semantic]", "语义节点筛查", 3);
 
 #endif
-                       if (errorNodes.Count > 0)
-                       {
-                           compilation = compilation.ReplaceSyntaxTree(tree, root.RemoveNodes(errorNodes, SyntaxRemoveOptions.KeepNoTrivia)!.SyntaxTree);
-                       }
-                       
+                        if (errorNodes.Count > 0)
+                        {
+                            compilation = compilation.ReplaceSyntaxTree(tree, root.RemoveNodes(errorNodes, SyntaxRemoveOptions.KeepNoTrivia)!.SyntaxTree);
+                        }
+
 #if DEBUG
-                       stopwatch.StopAndShowCategoreInfo("[Semantic]", "语义节点替换", 3);
+                        stopwatch.StopAndShowCategoreInfo("[Semantic]", "语义节点替换", 3);
 #endif
-                   }
+                    }
 
-               }
-               return compilation;
-           };
+                }
+                return compilation;
+            };
         }
 
     }
