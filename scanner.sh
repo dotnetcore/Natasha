9dca621f4bb14438d30dbe3a1a07c935183a7e66
AzulX <16236903+NMSAzulX@users.noreply.github.com>
2023-04-23 14:02:35 +0800

    Update scanner.sh

--- scanner.sh
+++ scanner.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-dotnet run --project "./test/workflow/Workflow.Initialization/Workflow.Initialization.csproj"
+dotnet run -c Release --project "./test/workflow/Workflow.Initialization/Workflow.Initialization.csproj"
