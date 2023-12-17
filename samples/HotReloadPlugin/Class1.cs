3477a1acd3d180051d6f614450457a7f459180dd
张木森 · 作霖 <16236903+NMSAzulX@users.noreply.github.com>
2023-12-17 16:46:27 +0800

    上传实验案例

---
+++ samples/HotReloadPlugin/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace HotReloadPlugin
+{
+    public class A
+    {
+        public int Code = 3;
+        [CLSCompliant(false)]
+        public void Show() { Console.WriteLine(Code); }
+        [CLSCompliant(false)]
+        public void Show2(int i) { Console.WriteLine(Code+i); }
+    }
+}
---
+++ samples/HotReloadSample/ReloadController.cs
@@ -0,0 +1,18 @@
+﻿#if DEBUG
+[assembly: System.Reflection.Metadata.MetadataUpdateHandlerAttribute(typeof(HotReloadSample.HotReloadService))]
+namespace HotReloadSample
+{
+    public static class HotReloadService
+    {
+#pragma warning disable CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
+        public static event Action<Type[]?>? UpdateApplicationEvent;
+#pragma warning restore CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
+
+        internal static void ClearCache(Type[]? types) { }
+        internal static void UpdateApplication(Type[]? types)
+        {
+            UpdateApplicationEvent?.Invoke(types);
+        }
+    }
+}
+#endif
