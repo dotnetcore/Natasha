5cb65326ad45bc655e738f64cf53f241ad3340f3
张木森 · 作霖 <16236903+NMSAzulX@users.noreply.github.com>
2024-07-01 15:52:16 +0800

    Update HotExecutorGenerator.cs

--- src/Natasha.CSharp/Extension/HotExecutor/Natasha.CSharp.HotExecutor.SG/HotExecutorGenerator.cs
+++ src/Natasha.CSharp/Extension/HotExecutor/Natasha.CSharp.HotExecutor.SG/HotExecutorGenerator.cs
@@ -29,38 +29,40 @@ HEProxy.ShowMessage = async msg => {
             var classNodes = syntaxTrees
               .SelectMany(t => t.GetRoot().DescendantNodes())
               .OfType<ClassDeclarationSyntax>().ToList();
-            // 检查是否有继承自 System.Windows.Forms.Form 的类
-            var hasWinFormFormClass = classNodes
-              .Any(c => c.BaseList != null && c.BaseList.Types.Any(t => t.Type.ToString() == "Form"));
-
-            if (hasWinFormFormClass && fileList.Any(item=>item.EndsWith(".Designer.cs")))
+            if (classNodes != null && classNodes.Count>0)
             {
-                // 这里可以根据判断结果执行相应的操作
-                // 例如生成特定的代码或输出信息
-                coreScript = $@"
+                // 检查是否有继承自 System.Windows.Forms.Form 的类
+                var hasWinFormFormClass = classNodes
+                  .Any(c => c.BaseList != null && c.BaseList.Types.Any(t => t.Type.ToString() == "Form"));
+
+                if (hasWinFormFormClass && fileList.Any(item => item.EndsWith(".Designer.cs")))
+                {
+                    // 这里可以根据判断结果执行相应的操作
+                    // 例如生成特定的代码或输出信息
+                    coreScript = $@"
 {winformAndwpfLoggerScript}
 HEProxy.SetProjectKind(HEProjectKind.Winform);
 HEProxy.ExcludeGlobalUsing(""System.Windows.Controls"");
 HEProxy.ExcludeGlobalUsing(""System.Windows"");
 DelegateHelper<System.Windows.Forms.FormCollection, System.Windows.Forms.Form>.GetDelegate(""Remove"", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
 ";
-            }
-            else
-            {
-
-                var hasWPFWindowClass = classNodes
-                  .Any(c => c.BaseList != null && c.BaseList.Types.Any(t => t.Type.ToString() == "Window"));
-
-                if (hasWPFWindowClass && fileList.Any(item => item.EndsWith(".xaml.cs")))
+                }
+                else
                 {
-                    coreScript = $@"
+
+                    var hasWPFWindowClass = classNodes
+                      .Any(c => c.BaseList != null && c.BaseList.Types.Any(t => t.Type.ToString() == "Window"));
+
+                    if (hasWPFWindowClass && fileList.Any(item => item.EndsWith(".xaml.cs")))
+                    {
+                        coreScript = $@"
 {winformAndwpfLoggerScript}
 HEProxy.SetProjectKind(HEProjectKind.WPF);
 HEProxy.ExcludeGlobalUsing(""System.Windows.Forms"");
 ";
+                    }
                 }
             }
-
 
             var nameSapce = context.Compilation.GetEntryPoint(cancellationToken: new System.Threading.CancellationToken())!.ContainingNamespace.Name;
             string proxyMethodContent = $@"
