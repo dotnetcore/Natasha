86eeec515fe1b4ff1f185b400d6a9d5c3f7cfcb7
张木森 · 作霖 <16236903+NMSAzulX@users.noreply.github.com>
2024-07-05 01:54:14 +0800

    优化和简化动态方法模板

--- src/Natasha.CSharp/Extension/Natasha.CSharp.Extension.MethodCreator/StringExtension.cs
+++ src/Natasha.CSharp/Extension/Natasha.CSharp.Extension.MethodCreator/StringExtension.cs
@@ -12,19 +12,39 @@ public static class StringExtension
         config?.Invoke(builder);
         return (script, [], builder);
     }
+
+
     public static (string script, string[] usings, AssemblyCSharpBuilder builder) WithAssemblyBuilder(this string script, Func<NatashaLoadContext, NatashaLoadContext> config)
     {
         AssemblyCSharpBuilder builder = new();
         builder.ConfigLoadContext(config);
         return (script, [], builder);
     }
+
+    public static (string script, string[] usings, AssemblyCSharpBuilder builder) WithSimpleBuilder(this string script)
+    {
+        AssemblyCSharpBuilder builder = new();
+        builder.UseRandomLoadContext();
+        builder.UseSimpleMode();
+        builder.ConfigLoadContext(ctx => ctx.AddReferenceAndUsingCode<object>());
+        return (script, [], builder);
+    }
+
+    public static (string script, string[] usings, AssemblyCSharpBuilder builder) WithMetadata<T>(in this (string script, string[] usings, AssemblyCSharpBuilder builder) buildInfo)
+    {
+        return (buildInfo.script, buildInfo.usings, buildInfo.builder.ConfigLoadContext(ctx => ctx.AddReferenceAndUsingCode(typeof(T))));
+    }
+    public static (string script, string[] usings, AssemblyCSharpBuilder builder) WithMetadata(in this (string script, string[] usings, AssemblyCSharpBuilder builder) buildInfo, Type type)
+    {
+        return (buildInfo.script, buildInfo.usings, buildInfo.builder.ConfigLoadContext(ctx => ctx.AddReferenceAndUsingCode(type)));
+    }
     public static (string script, string[] usings, AssemblyCSharpBuilder builder) WithUsings(in this (string script, string[] usings, AssemblyCSharpBuilder builder) buildInfo,params string[] usings)
     {
         return (buildInfo.script, usings, buildInfo.builder);
     }
-    public static T? ToDelegate<T>(in this (string script, string[] usings, AssemblyCSharpBuilder builder) buildInfo, string modifier = "") where T: Delegate
+    public static T ToDelegate<T>(in this (string script, string[] usings, AssemblyCSharpBuilder builder) buildInfo, string modifier = "") where T: Delegate
     {
-
+        var scriptBuilder = buildInfo.builder;
         var className = $"N{Guid.NewGuid():N}";
         var methodInfo = typeof(T).GetMethod("Invoke")!;
 
@@ -34,18 +54,22 @@ public static class StringExtension
         var methodParameters = methodInfo.GetParameters();
         for (int i = 0; i < methodParameters.Length; i+=1)
         {
-            parameterScript.Append($"{methodParameters[i].ParameterType.GetDevelopName()} arg{i+1},");
+            var paramType = methodParameters[i].ParameterType;
+            scriptBuilder.ConfigLoadContext(ctx => ctx.AddReferenceAndUsingCode(paramType));
+            parameterScript.Append($"{paramType.GetDevelopName()} arg{i+1},");
         }
         if (parameterScript.Length > 0)
         {
             parameterScript.Length -= 1;
         }
+        
         StringBuilder usingCode = new();
         foreach (var item in buildInfo.usings)
 	    {
+           
             usingCode.AppendLine($"using {item};");
-
-        }               
+            
+        }
         buildInfo.builder.Add($"{usingCode} public static class {className} {{ public static {(modifier ?? string.Empty)} {returnTypeScript} Invoke({parameterScript}){{ {buildInfo.script} }} }}");
         var asm = buildInfo.builder.GetAssembly();
         var type = asm.GetType(className);
@@ -53,17 +77,17 @@ public static class StringExtension
         {
                return (T)Delegate.CreateDelegate(typeof(T), type.GetMethod("Invoke")!);
         }
-        return null;
+        throw new Exception($"未找到 {className} 类型！");
     }
-    public static T? ToAsyncDelegate<T>(in this (string script, string[] usings, AssemblyCSharpBuilder builder) buildInfo) where T : Delegate
+    public static T ToAsyncDelegate<T>(in this (string script, string[] usings, AssemblyCSharpBuilder builder) buildInfo) where T : Delegate
     {
         return ToDelegate<T>(buildInfo, "async");
     }
-    public static T? ToUnsafeDelegate<T>(in this (string script, string[] usings, AssemblyCSharpBuilder builder) buildInfo) where T : Delegate
+    public static T ToUnsafeDelegate<T>(in this (string script, string[] usings, AssemblyCSharpBuilder builder) buildInfo) where T : Delegate
     {
         return ToDelegate<T>(buildInfo, "unsafe");
     }
-    public static T? ToUnsafeAsyncDelegate<T>(in this (string script, string[] usings, AssemblyCSharpBuilder builder) buildInfo) where T : Delegate
+    public static T ToUnsafeAsyncDelegate<T>(in this (string script, string[] usings, AssemblyCSharpBuilder builder) buildInfo) where T : Delegate
     {
         return ToDelegate<T>(buildInfo, "unsafe async");
     }
