d2f9f542e0dbef562fe7edd3bf0a030b826a40cb
张木森 · 作霖 <16236903+NMSAzulX@users.noreply.github.com>
2024-07-28 13:22:23 +0800

    update loadcontext

---
+++ src/Natasha.CSharp/Natasha.CSharp.Compiler/Component/Metadata/NatashaLoadContext.Usings.cs
@@ -0,0 +1,26 @@
+﻿using Natasha.CSharp.Compiler.Component;
+using System.Collections.Generic;
+using System.Reflection;
+
+public sealed partial class NatashaLoadContext
+{
+
+    public NatashaLoadContext AddUsings(HashSet<string> usings, AssemblyCompareInformation compareInfomation = AssemblyCompareInformation.None)
+    {
+        UsingRecorder.Using(usings);
+        return this;
+    }
+    public NatashaLoadContext AddUsings(Assembly assembly, AssemblyCompareInformation compareInfomation = AssemblyCompareInformation.None)
+    {
+        var result = MetadataHelper.GetNamespaceFromMemroy(assembly, null);
+        if (result != null)
+        {
+            AddUsings(result);
+        }
+        return this;
+    }
+    public NatashaLoadContext AddUsings<T>(AssemblyCompareInformation compareInfomation = AssemblyCompareInformation.None)
+    {
+        return AddUsings(typeof(T).Assembly);
+    }
+}
--- src/Natasha.CSharp/Natasha.CSharp.Compiler/Component/Metadata/NatashaLoadContext.cs
+++ src/Natasha.CSharp/Natasha.CSharp.Compiler/Component/Metadata/NatashaLoadContext.cs
@@ -14,7 +14,7 @@ public static class NatashaLoadContext<TCreator> where TCreator : INatashaDynami
     }
     public static void Prepare() { }
 }
-public sealed class NatashaLoadContext : IDisposable
+ public sealed partial class NatashaLoadContext : IDisposable
 {
     public INatashaDynamicLoadContextBase Domain;
     private static readonly object _initLock = new();
@@ -64,8 +64,6 @@ public sealed class NatashaLoadContext : IDisposable
                 }
             }
         }
-        
-        
     }
 
     public static NatashaLoadContext DefaultContext = default!;
@@ -91,87 +89,16 @@ public sealed class NatashaLoadContext : IDisposable
     /// </summary>
     public readonly NatashaUsingCache UsingRecorder;
 
-    internal void LoadMetadataWithAssembly(Assembly assembly)
-    {
-        var result = MetadataHelper.GetMetadataAndNamespaceFromMemory(assembly);
-        if (result.HasValue)
-        {
-            AddReferenceAndUsingCode(result.Value.asmName, result.Value.metadata, result.Value.namespaces);
-        }
-    }
-
-
-    public NatashaLoadContext AddReferenceAndUsingCode(AssemblyName name, MetadataReference metadataReference, HashSet<string> usings, AssemblyCompareInformation compareInfomation = AssemblyCompareInformation.None)
+ 
+    public NatashaLoadContext AppendReference
+        (AssemblyName name, MetadataReference metadataReference, AssemblyCompareInformation compareInfomation = AssemblyCompareInformation.None)
     {
         ReferenceRecorder.AddReference(name, metadataReference, compareInfomation);
+        return this;
+    }
+    public NatashaLoadContext AppendUsings(HashSet<string> usings)
+    {
         UsingRecorder.Using(usings);
-        return this;
-    }
-
-
-    /// <summary>
-    /// 根据 实现程序集， 增加元数据 和 using
-    /// </summary>
-    /// <param name="assembly">程序集</param>
-    /// <param name="excludeAssembliesFunc">过滤委托</param>
-    /// <param name="loadReferenceBehavior">加载行为</param>
-    public NatashaLoadContext AddReferenceAndUsingCode(Assembly assembly, Func<AssemblyName, bool>? excludeAssembliesFunc = null, AssemblyCompareInformation loadReferenceBehavior = AssemblyCompareInformation.None)
-    {
-        var result = MetadataHelper.GetMetadataAndNamespaceFromMemory(assembly, null);
-        if (result.HasValue)
-        {
-            AddReferenceAndUsingCode(result.Value.asmName, result.Value.metadata, result.Value.namespaces, loadReferenceBehavior);
-            var assmblies = Creator.GetDependencyAssemblies(assembly);
-            if (assmblies != null && assmblies.Any())
-            {
-                if (excludeAssembliesFunc != null)
-                {
-                    foreach (var depAssembly in assmblies)
-                    {
-                        var asmName = depAssembly.GetName();
-                        if (!ReferenceRecorder.HasReference(asmName) && !excludeAssembliesFunc(asmName))
-                        {
-                            result = MetadataHelper.GetMetadataAndNamespaceFromMemory(depAssembly, null);
-                            if (result.HasValue)
-                            {
-                                AddReferenceAndUsingCode(asmName, result.Value.metadata, result.Value.namespaces, loadReferenceBehavior);
-                            }
-                        }
-                    }
-                }
-                else
-                {
-                    foreach (var depAssembly in assmblies)
-                    {
-                        var asmName = depAssembly.GetName();
-                        if (!ReferenceRecorder.HasReference(asmName))
-                        {
-                            result = MetadataHelper.GetMetadataAndNamespaceFromMemory(depAssembly, null);
-                            if (result.HasValue)
-                            {
-                                AddReferenceAndUsingCode(asmName, result.Value.metadata, result.Value.namespaces, loadReferenceBehavior);
-                            }
-                        }
-                    }
-                }
-
-            }
-        }
-        return this;
-    }
-
-    /// <summary>
-    /// 根据 引用程序集所在的文件， 增加元数据 和 using
-    /// </summary>
-    /// <param name="path">文件路径</param>
-    /// <param name="loadReferenceBehavior">加载行为</param>
-    public NatashaLoadContext AddReferenceAndUsingCode(string path, AssemblyCompareInformation loadReferenceBehavior = AssemblyCompareInformation.None)
-    {
-        var result = MetadataHelper.GetMetadataAndNamespaceFromFile(path, null);
-        if (result.HasValue)
-        {
-            AddReferenceAndUsingCode(result.Value.asmName, result.Value.metadata, result.Value.namespaces, loadReferenceBehavior);
-        }
         return this;
     }
 
